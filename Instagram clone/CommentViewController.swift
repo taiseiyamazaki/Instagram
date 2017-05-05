47a5f0fe2a782cc9814be35f269483be308e2f0c
山崎大聖 <yamazakitaisei@yamazakitaisei-no-macbook-pro.local>
2017-05-05 19:30:15 +0900

    完成

---
+++ Instagram clone/CommentViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CommentViewController.swift
+//  Instagram clone
+//
+//  Created by 山崎大聖 on 2017/04/26.
+//  Copyright © 2017年 山崎大聖. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import SVProgressHUD
+
+class CommentViewController: UIViewController {
+    
+    var commentData = [[String:String]]()
+    
+    var postData:PostData!
+    
+   var commentArray:[String] = []
+    
+    @IBOutlet weak var commentTextField: UITextField!
+    
+    @IBOutlet weak var backPageButton: UIButton!
+
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+
+
+    }
+    @IBAction func backBtn(_ sender: Any) {
+        let postRef = FIRDatabase.database().reference().child(Const.PostPath).child(postData.id!)
+        let commentData = ["name": (FIRAuth.auth()?.currentUser?.displayName)!, "comment": commentTextField.text! ]
+        postData.comments.append(commentData)
+        let comments = ["comments": postData.comments]
+        postRef.updateChildValues(comments)
+            }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
